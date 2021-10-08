9cc2e7ae60caf49e4163dedbf46018b41739ffb4
Santosh <pantsantosh23@gmail.com>
2021-10-08 12:34:53 +0545

    Final Version of Game

--- guess_the_number.c
+++ guess_the_number.c
@@ -19,7 +19,7 @@ a:
 int main()
 {
 
-    int n = 1, i, move, called;
+    int n = 7, i, move, called;
     called = myrand();
     printf("********************************************************\n");
     printf("\t\t\tGame Time \n");
@@ -27,10 +27,10 @@ int main()
 
     printf("Here Computer guessd a number less then 100. \n Now it's your time to match the number in 7 moves.\n");
 
-    while (n <= 7)
+    while (n > 0)
     {
-        printf("\nGuess your %d moves : ", n);
-        n++;
+        printf("\nGuess the number in  %d moves : ", n);
+        n--;
 
         scanf("%d", &move);
 
@@ -41,15 +41,15 @@ int main()
         }
         else if (move < called)
         {
-            printf("\nYour Number is smaller. Enter a Greater Number --->");
+            printf("\nYour Number is smaller. Enter a Greater Number --->\n");
         }
         else
             printf("\nYour Number is Greater. Now enter a smaller Number --->");
 
-        if (n > 7)
+        if (n < 1)
         {
-            printf("Sorry You Lose. Try Again for the nest time. Dimag laga bacca : :( ");
-            printf("\nComputer gussed a %d Number ", called);
+            printf("\n\nSorry You Lose. Try Again for the next time.");
+            printf("\nThe Number that computer guessed was %d", called);
             break;
         }
     }
